acaebc94de8f97db466ff096698f235339169fc2
junaidfkhan786 <junaidfkhan786@gmail.com>
2020-10-24 16:13:44 +0530

    some changes

--- src/app/models/api.model.ts
+++ src/app/models/api.model.ts
@@ -1,8 +1,8 @@
 import { HttpHeaders } from '@angular/common/http'
 
 export const API_URL = "http://localhost:5000/api"
-// export const API_LIVE = "https://booksbyweight.herokuapp.com/api"
-export const API_LIVE = "http://localhost:5000/api"
+export const API_LIVE = "https://booksbyweight.herokuapp.com/api"
+// export const API_LIVE = "http://localhost:5000/api"
 
 
 export const httpOptions = {
